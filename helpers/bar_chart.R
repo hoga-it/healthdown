7adb46684937427722e89261a19a26bebd0164c0
Andreas <andreas.hofheinz@outlook.com>
2021-04-19 11:39:49 +0200

    Added axis labels to plots. Relates to #4

--- helpers/bar_chart.R
+++ helpers/bar_chart.R
@@ -1,14 +1,17 @@
 create_bar_chart <- function(curr_sel_data, prim_var) {
   df <- curr_sel_data
   
-  if(nrow(df) > 0) {
+  if (nrow(df) > 0) {
     df$name <- ifelse(is.na(df$NAME_2), as.character(df$ST), as.character(df$NAME_2))
     
     df %>% 
       group_by(name) %>%
       e_charts(name, stack = "grp") %>%
-      e_bar_(prim_var, ) %>%
+      e_bar_(prim_var) %>%
+      e_axis_labels(y = prim_var) %>% 
+      e_y_axis(nameLocation = "center")  %>% 
       e_flip_coords()
+      
   } else {
     # TODO add message to select a shape
   }
--- helpers/scatter_plot.R
+++ helpers/scatter_plot.R
@@ -1,13 +1,20 @@
 create_scatter_plot <- function(curr_sel_data, prim_var, sec_var) {
   df <- curr_sel_data
   
-  if(nrow(df) > 0) {
+  if (nrow(df) > 0) {
     df$name <- ifelse(is.na(df$NAME_2), as.character(df$ST), as.character(df$NAME_2))
     
     df %>% 
       group_by(name) %>%
       e_charts_(prim_var) %>% 
-      e_scatter_(sec_var, symbol_size = 15)
+      e_scatter_(sec_var, symbol_size = 15) %>% 
+      e_axis_labels(
+        x = prim_var,
+        y = sec_var
+      ) %>% 
+      e_x_axis(nameLocation = "center") %>% 
+      e_y_axis(nameLocation = "center")
+      
   } else {
     # TODO add message to select a shape
   }
