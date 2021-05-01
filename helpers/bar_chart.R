acae550568f1a2e39a61e419ea3bcfcc299d62f8
Peter Gandenberger <peter.gandenberger@gmail.com>
2021-05-01 14:31:21 +0200

    #4 added placeholder for empty graphs

--- helpers/bar_chart.R
+++ helpers/bar_chart.R
@@ -1,16 +1,16 @@
 create_bar_chart <- function(df, prim_var) {
-  if (nrow(df) > 0) {
-    df$name <- ifelse(is.na(df$NAME_2), as.character(df$ST), as.character(df$NAME_2))
-    
-    df %>% 
-      group_by(name) %>%
-      e_charts(name, stack = "grp") %>%
-      e_bar_(prim_var) %>%
-      e_axis_labels(y = prim_var) %>% 
-      e_y_axis(nameLocation = "center", nameGap  = 30)  %>% 
-      e_flip_coords()
-      
-  } else {
-    # TODO add message to select a shape
-  }
+  
+  validate(
+    need(nrow(df) > 0, "Select regions on the map to add their values to this graph.")
+  )
+  
+  df$name <- ifelse(is.na(df$NAME_2), as.character(df$ST), as.character(df$NAME_2))
+  
+  df %>% 
+    group_by(name) %>%
+    e_charts(name, stack = "grp") %>%
+    e_bar_(prim_var) %>%
+    e_axis_labels(y = prim_var) %>% 
+    e_y_axis(nameLocation = "center", nameGap  = 30)  %>% 
+    e_flip_coords()
 }
--- helpers/scatter_plot.R
+++ helpers/scatter_plot.R
@@ -1,24 +1,24 @@
 create_scatter_plot <- function(df, prim_var, sec_var) {
-  if (nrow(df) > 0) {
-    df$name <- ifelse(is.na(df$NAME_2), as.character(df$ST), as.character(df$NAME_2))
+  validate(
+    need(nrow(df) > 0, "Select regions on the map to add their values to this graph.")
+  )
+  
+  df$name <- ifelse(is.na(df$NAME_2), as.character(df$ST), as.character(df$NAME_2))
 
-    df %>%
-      group_by(name) %>%
-      e_charts_(prim_var) %>%
-      e_scatter_(sec_var, symbol_size = 15) %>%
-      e_axis_labels(
-        x = prim_var,
-        y = sec_var
-      ) %>%
-      e_x_axis(
-        nameLocation = "center", nameGap = 30,
-        min = floor(0.9 * min(df[[prim_var]]))
-      ) %>%
-      e_y_axis(
-        nameLocation = "center", nameGap = 50,
-        min = floor(0.9 * min(df[[sec_var]]))
-      )
-  } else {
-    # TODO add message to select a shape
-  }
+  df %>%
+    group_by(name) %>%
+    e_charts_(prim_var) %>%
+    e_scatter_(sec_var, symbol_size = 15) %>%
+    e_axis_labels(
+      x = prim_var,
+      y = sec_var
+    ) %>%
+    e_x_axis(
+      nameLocation = "center", nameGap = 30,
+      min = floor(0.9 * min(df[[prim_var]]))
+    ) %>%
+    e_y_axis(
+      nameLocation = "center", nameGap = 50,
+      min = floor(0.9 * min(df[[sec_var]]))
+    )
 }
